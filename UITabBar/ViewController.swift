b202aae28b2ccf774dd21ad32932d5db07a091c4
Ruslan <ruslanfilistovich@gmail.com>
2019-08-13 18:41:23 +0300

    Initial Commit

---
+++ UITabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by Ruslan on 8/13/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
